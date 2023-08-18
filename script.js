5016cb26f7648e20e5fc55db851ec01acdfacc7e
Anissa7 <a.ouedraogo@alustudent.com>
2023-08-18 23:29:34 +0200

    the app is now there

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-const weatherAPIUrl = "'https://open-weather-map27.p.rapidapi.com/weather'" ;
-const weatherAPIKey = "696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0";
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://drug-info-and-price-history.p.rapidapi.com/1/genericname?drug=advil',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0',
+		'X-RapidAPI-Host': 'drug-info-and-price-history.p.rapidapi.com'
+	}
+};
 
-const tripAdvisorAPIUrl = "https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchAirport";
-const tripAdvisorAPIKey = "696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0";
-
-const cityForm = document.querySelector("#cityForm");
-const cityInput = document.querySelector("#cityInput");
-const weatherDiv = document.querySelector("#weather");
-const hotelsDiv = document.querySelector("#hotels");
-
-cityForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const city = cityInput.value;
-
-    // Fetch weather data
-    const weatherResponse = await fetch(`${weatherAPIUrl}?q=${city}&appid=${weatherAPIKey}`);
-    const weatherData = await weatherResponse.json();
-    // Process weather data and display it in weatherDiv
-
-    // Fetch hotel data
-    const hotelsResponse = await fetch(`${tripAdvisorAPIUrl}/${city}/hotels?key=${tripAdvisorAPIKey}`);
-    const hotelsData = await hotelsResponse.json();
-    // Process hotel data and display it in hotelsDiv
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
