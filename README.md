af1c42323da745545e248ead679164101ab865bf
Anissa7 <a.ouedraogo@alustudent.com>
2023-08-19 03:49:25 +0200

    Done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Medicine Information App
 
 Welcome to the Medicine Information App! This is a simple web application that allows users to enter a medicine name and retrieve its generic name using an API. The app is built using HTML, CSS, and JavaScript, and it utilizes jQuery for making AJAX requests.
+Here is the link to the demo :
--- script.js
+++ script.js
@@ -1,14 +1,84 @@
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://drug-info-and-price-history.p.rapidapi.com/1/genericname?drug=advil',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0',
-		'X-RapidAPI-Host': 'drug-info-and-price-history.p.rapidapi.com'
-	}
-};
+$(document).ready(function() {
+    const statsTable = $("#stats-table");
+    const countryInput = $("#country-input");
+    const searchButton = $("#search-button");
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+    // Function to populate the table with data
+    function populateTable(data) {
+        statsTable.find("tbody").empty(); // Clear existing rows
+        
+        const countryData = data.response[0]; // Assuming the API returns an array of data
+        const newRow = $("<tr>").html(`
+            <td>${countryData.country}</td>
+            <td>${countryData.cases.total}</td>
+            <td>${countryData.deaths.total}</td>
+            <td>${countryData.cases.recovered}</td>
+        `);
+        statsTable.append(newRow);
+    }
+
+    // Function to fetch and display data for a specific country
+    function searchCountryData(countryName) {
+        const settings = {
+            async: true,
+            crossDomain: true,
+            url: `https://covid-193.p.rapidapi.com/history?country=${countryName}`,
+            method: "GET",
+            headers: {
+                "X-RapidAPI-Key": "696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0",
+                "X-RapidAPI-Host": "covid-193.p.rapidapi.com"
+            }
+        };
+
+        $.ajax(settings).done(function(response) {
+            if (response.response) {
+                populateTable(response);
+            } else {
+                alert("Country not found or data not available.");
+            }
+        });
+    }
+
+    searchButton.on("click", function() {
+        const userInput = countryInput.val().trim();
+        if (userInput !== "") {
+            searchCountryData(userInput);
+        } else {
+            alert("Please enter a valid country name.");
+        }
+    });
+    
+    // Fetch data from the second API
+    const secondSettings = {
+        async: true,
+        crossDomain: true,
+        url: "https://covid-193.p.rapidapi.com/statistics",
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0",
+            "X-RapidAPI-Host": "covid-193.p.rapidapi.com"
+        }
+    };
+
+    $.ajax(secondSettings).done(function(response) {
+        console.log(response);
+    });
+    
+    // Fetch data from the third API
+    const thirdSettings = {
+        async: true,
+        crossDomain: true,
+        url: "'https://covid-193.p.rapidapi.com/history?",
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "696cc32825mshf220551f0404d51p1201eajsn651d61b40bf0",
+			"X-RapidAPI-Host": "covid-193.p.rapidapi.com"
+            
+        }
+    };
+
+    $.ajax(thirdSettings).done(function(response) {
+        console.log(response);
+        // Process and display the response as needed
+    });
 });
